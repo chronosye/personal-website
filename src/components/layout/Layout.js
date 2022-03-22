547fe88eb3989f718dbba16577574b2ec170ab6c
Uldis Jansons <jansons08@gmail.com>
2022-03-22 17:34:33 +0200

    edited responsivness and not found page

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -2,12 +2,14 @@ import { Fragment } from 'react';
 
 import classes from './Layout.module.scss';
 import Navbar from '../navbar/Navbar';
+import Footer from '../footer/Footer';
 
 const Layout = (props) => {
   return (
     <Fragment>
       <Navbar/>
       <main className={classes.main}>{props.children}</main>
+      <Footer/>
     </Fragment>
   );
 };
--- src/components/notFound/NotFound.js
+++ src/components/notFound/NotFound.js
@@ -4,8 +4,7 @@ import classes from "./NotFound.module.scss";
 const NotFound = () => {
   return (
     <div className={classes.text}>
-      <h1>404</h1>
-      <h4>Page not found :(</h4>
+      <p>Page not found :(</p>
     </div>
   );
 };
