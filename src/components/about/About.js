54cdd9e0ba56849498034ce97461b58eb4c152d7
Uldis Jansons <jansons08@gmail.com>
2022-03-23 16:33:36 +0200

    added about me page

---
+++ src/components/about/About.js
@@ -0,0 +1,35 @@
+import React from "react";
+import classes from "./About.module.scss";
+import TwoSideGrid from "../layout/TwoSideGrid";
+import AboutImage from "../../assets/aboutMe.gif";
+
+const About = () => {
+  const text = (
+    <div className={classes.text}>
+      <p>
+        I am a programmer from Latvia. Right now I have my final semester at the
+        University of Latvia in computer sciences. I like to create UI's that
+        are easier to use and also looks great.
+      </p>
+      <p>
+        Trying to pay attention to every single detail and deliver things in
+        time. Also, I am trying to help others when they need some help.
+      </p>
+      <p>
+        Besides programming, I am playing handball. But in my free time, I love
+        to do other sports - basketball, volleyball, alpine skiing, ice skating.
+        In other words, I don't like to sit and do nothing.
+      </p>
+    </div>
+  );
+
+  const image = (
+    <div className={classes.image}>
+      <img src={AboutImage} alt="About" />
+    </div>
+  );
+
+  return <TwoSideGrid text={text} image={image} />;
+};
+
+export default About;
